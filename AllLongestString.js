e2fa00132152b3ef53bb795ac79350370d4cfd78
github username <mwbritt81@gmail.com>
2020-09-09 10:05:26 -0500

    AllLongestStrings solution

--- AllLongestString.js
+++ AllLongestString.js
@@ -4,3 +4,28 @@ Example
 
 For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
 allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
+
+
+function allLongestStrings(inputArray) {
+    'use strict';
+    let maxSize = Math.max(...inputArray.map(x => x.length));
+    return inputArray.filter(x => x.length === maxSize);
+}
+
+
+function allLongestStrings(inputArray) {
+    let longest = [];
+    let stringLength = [];
+    let maxNum;
+    for (let s of inputArray) {
+      stringLength.push(s.length);
+      maxNum = Math.max(...stringLength)
+    }
+    for (let s of inputArray) {
+      if (s.length === maxNum) {
+        longest.push(s)
+      }
+    }
+    return longest;
+}
+allLongestStrings(["aba", "aa", "ad", "vcd", "aba"])
