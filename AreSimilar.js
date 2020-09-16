5c886fc81439d82650f64e2284abb76c001a4d9a
github username <mwbritt81@gmail.com>
2020-09-16 11:37:14 -0500

    Innolitics solution

--- AreSimilar.js
+++ AreSimilar.js
@@ -23,4 +23,11 @@ function areSimilar(a,b) {
     if (b1.length === 2 && a1.toString() === b1.toString())   return true;
     return false;
 }
+
+function areSimilar(a, b) {
+    const ad = a.filter((v,i)=>v!=b[i])
+    const bd = b.filter((v,i)=>v!=a[i])
+    return ad.length == 0 || (ad.length == 2 && ad.join('') == bd.reverse().join(''))
+}
+
 areSimilar([1,2,3],[2,1,3])
--- Innolitics.js
+++ Innolitics.js
@@ -1,3 +1,33 @@
 Write a function that counts the number of times the numeral “7” occurs in a passed in integer value.
 
+function numbers7(integer) {
+    let count = 0;
+    let int = integer.toString();
+
+  for (let i of int) {
+    console.log(i)
+    if (i === '7') count++;
+  }
+     
+    
+    return count;
+  }
+  numbers7(12727) returns 2
+
 Write the same function without converting the input into a string.
+
+function numbers7(integer) {
+    let count = 0;
+    let len = Math.ceil(Math.log(integer + 1) / Math.LN10);
+    let x = 1;
+     //console.log(Math.floor(integer/1000 % 10))
+    while (len > 0) {
+     let i = Math.floor((integer/x) % 10);
+     if (i === 7) count++;
+     len--;
+     x *= 10;
+    }
+     
+    
+    return count;
+  }
