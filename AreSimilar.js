c16900c0ed6e6ed74d2226693e09d2303cf1e6be
github username <mwbritt81@gmail.com>
2020-09-15 20:34:21 -0500

    AreSimilar

---
+++ AreSimilar.js
@@ -0,0 +1,15 @@
+Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements in one of the arrays.
+
+Given two arrays a and b, check whether they are similar.
+
+Example
+
+For a = [1, 2, 3] and b = [1, 2, 3], the output should be
+areSimilar(a, b) = true.
+
+The arrays are equal, no need to swap any elements.
+
+function areSimilar(a,b) {
+
+}
+areSimilar([1,2,3],[2,1,3])
---
+++ Innolitics.js
@@ -0,0 +1,3 @@
+Write a function that counts the number of times the numeral “7” occurs in a passed in integer value.
+
+Write the same function without converting the input into a string.
